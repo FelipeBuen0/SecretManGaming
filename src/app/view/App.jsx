32cf326dba02427ca0164943b0ae732682c9bd5f
Felipe Bueno <f.dacunhabueno@gmail.com>
2023-08-27 17:09:04 -0300

    feat(routes): :sparkles: Router added to application
    #1

--- src/app/view/App.jsx
+++ src/app/view/App.jsx
@@ -1,15 +1,17 @@
 import React from "react"
 import Menu from "./components/menu/Menu"
+import { BrowserRouter } from  'react-router-dom'
 import Content from "./content/Content"
 import "./App.css"
 
 export default function App() {
   return (
-    <>
      <div className="App">
-        <Menu></Menu>
-        <Content></Content>
+        <BrowserRouter>
+        
+          <Menu></Menu>
+          <Content></Content>
+        </BrowserRouter>
      </div>
-    </>
   )
 }
